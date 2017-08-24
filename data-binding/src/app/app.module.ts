38dc3d8d39ebec5c2dd6e278daf23239e9534bab
AndreTimm <andretimm2012@gmail.com>
2017-08-24 12:56:53 -0300

    Output Property

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { InputPropertyComponent } from './input-property/input-property.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
-    InputPropertyComponent
+    InputPropertyComponent,
+    OutputPropertyComponent
   ],
   imports: [
     BrowserModule,
--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -58,6 +58,10 @@ export class DataBindingComponent implements OnInit {
     this.isMouseOver = !this.isMouseOver;
   }
 
+  onMudouValor(evento){
+    console.log(evento.novoValor);
+  }
+
   constructor() { }
 
   ngOnInit() {
